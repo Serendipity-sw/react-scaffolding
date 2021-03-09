0a02724963886a92461bd05a0d632820f3be3efe
sw.gloomysw@gmail.com <sw.gloomysw@gmail.com>
2021-03-09 20:57:04 +0800

    :construction: 增加px转rem

--- postcss.config.js
+++ postcss.config.js
@@ -19,5 +19,14 @@ module.exports = {
     require('postcss-short'),
     require('postcss-assets'),
     require('postcss-font-magician')({}),
+    require('postcss-pxtorem')({
+      rootValue: 100,
+      unitPrecision: 5,
+      propList: ['*'],
+      selectorBlackList: [],
+      replace: true,
+      mediaQuery: false,
+      minPixelValue: 0
+    })
   ]
 };
